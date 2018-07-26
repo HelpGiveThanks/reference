2d0e61265a0ee19c0872818833be0bf2fb8bd9c0
David Matson <githubdavid@gmail.com>
2018-07-26 14:44:41 -0700

    Inital move of old Reference files into its own repository
    I removed the reference folders from both the library and actionlog repositories.  This database needs to be in its own reporistory, which it now is.

---
+++ scripts/showTools.c
@@ -0,0 +1,4 @@
+show tools
+Show/Hide Status Area
+[ Show ]
+December 11, ଘ౮28 21:00:04 reference.fp7 - show tools -1-
---
+++ scripts/start.c
@@ -0,0 +1,6 @@
+start
+Show/Hide Status Area
+[ Lock; Hide ]
+Show/Hide Text Ruler
+[ Hide ]
+December 11, ଘ౮28 21:02:21 reference.fp7 - start -1-
