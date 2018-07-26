106ccb737d2f3772276162f2f01d447e1240a15d
David Matson <githubdavid@gmail.com>
2018-07-26 14:52:36 -0700

    This should bring the reference database up to date!

--- scripts/showTools.c
+++ scripts/showTools.c
@@ -1,4 +1,6 @@
-show tools
-Show/Hide Status Area
-[ Show ]
-December 11, ଘ౮28 21:00:04 reference.fp7 - show tools -1-
+July 26, 2018 14:46:47 reference.fmp12 - start -1-
+start
+Show/Hide Toolbars
+[ Lock; Hide ]
+Show/Hide Text Ruler
+[ Hide ]
--- scripts/start.c
+++ scripts/start.c
@@ -1,6 +1,6 @@
+July 26, 2018 14:46:47 reference.fmp12 - start -1-
 start
-Show/Hide Status Area
+Show/Hide Toolbars
 [ Lock; Hide ]
 Show/Hide Text Ruler
 [ Hide ]
-December 11, ଘ౮28 21:02:21 reference.fp7 - start -1-
